dc7fed01549766c5166634ac3d0bd852c328f7f5
yurey <aitaliabdelwahed@gmail.com>
2021-08-12 00:11:18 +0000

    redesign

--- personal_/__init__.py
+++ personal_/__init__.py
@@ -6,8 +6,7 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hwxqgwisgorxov:ef788a743b9b4c0cd306bc335287f0d3f7c84c8d26b1fef2e6c842635911b735@ec2-54-145-224-156.compute-1.amazonaws.com:5432/d1kuss1n29ut3c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hwxqgwisgorxov:ef788a743b9b4c0cd306bc335287f0d3f7c84c8d26b1fef2e6c842635911b735@ec2-54-145-224-156.compute-1.amazonaws.com:5432/d1kuss1n29ut3c'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']='$2b$12$wUSYAe3hTC6QdaYe2JdrZO8yk/hM4BhXGAYhCZBetA0CKQAxQfNH2'
 
